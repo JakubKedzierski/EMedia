a1792c63c1dd874846ee934712013c8b599b35e2
Jakub Kedzierski <248915@student.pwr.edu.pl>
2021-05-26 12:30:34 +0200

    main test deleted

--- pngParser/main.py
+++ pngParser/main.py
@@ -11,43 +11,7 @@ import random
 
 
 def main():
-    """
-    #gui = PNGParserGui()
-    data = [0, 1,234,123,1,253]
 
-    n,e,d = generate_keys()
-    public_key = (n,e)
-    private_key = (n,d)
-
-    size_of_block = 2  # rozmiar bloku w bajtach
-    chunk_size = size_of_block
-    blocks = []
-    for i in range(0,len(data),size_of_block):
-        chunk_to_enrypt_hex = bytes(data[i: i + size_of_block])
-        cipher_int = pow(int.from_bytes(chunk_to_enrypt_hex, 'big'), e, n)
-        block = cipher_int.to_bytes(n.bit_length(), 'big')
-        for j in range(0,len(block)):
-            blocks.append(block[j])
-
-
-    data_to_decrypt = blocks
-    print(blocks)
-    size_of_block = n.bit_length()
-
-    pixels = []
-    for i in range(0, len(data_to_decrypt), size_of_block):
-        chunk_to_decrypt_hex = bytearray(data_to_decrypt[i: i + size_of_block])
-        plain_text = pow(int.from_bytes(chunk_to_decrypt_hex, 'big'), d, n)
-        block = plain_text.to_bytes(2, 'big')
-        for i in range(0,chunk_size):
-            pixels.append(block[i:i+1])
-
-    for pixel in pixels:
-        print(int.from_bytes(pixel, 'big'))
-
-   # wykład godzina 1.08
-
-    """
 
     png_parser = PngFileParser()
     img = 'tux.png'
